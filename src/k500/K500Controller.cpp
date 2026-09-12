cf2f57d7a27010564b4b30ac69919486d031052d
Ari Sulistiono <ari.sulistiono@gmail.com>
2026-09-12 11:21:33 +0700

    fix(protocol): route six-way music source selection

--- src/k500/K500Controller.cpp
+++ src/k500/K500Controller.cpp
@@ -111,6 +111,7 @@ void K500Controller::hydrateFromDeviceMemory(const QByteArray &memory)
     m_music.digitalGainDb = static_cast<int>(fileU8(memory, 0x0022, 8)) - 12;
 
     m_mic.topMicVol = fileU8(memory, 0x0009, 35);
+    m_mic.musicInitVol = m_music.musicInitVol;
     m_mic.micInitVol = fileU8(memory, 0x0012, 25);
     m_mic.micAVol = fileU8(memory, 0x0014, 96);
     m_mic.micBVol = fileU8(memory, 0x0015, 96);
@@ -288,6 +289,7 @@ void K500Controller::handleStateEdit(const QString &path, const QVariant &value)
 
     bool isTopMusicPath = true;
     if (path == QStringLiteral("system.topMusicVol")) m_music.topMusicVol = qRound(value.toDouble());
+    else if (path == QStringLiteral("music.sourceRaw")) m_music.sourceRaw = qBound(0, value.toInt(), 5);
     else if (path == QStringLiteral("music.key")) m_music.key = value.toInt();
     else if (path == QStringLiteral("music.input1GainDb")) m_music.input1GainDb = value.toDouble();
     else if (path == QStringLiteral("music.input2GainDb")) m_music.input2GainDb = value.toDouble();
